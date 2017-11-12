52e44a7b2e6380b8039feb2ee78328bc0e4b745c
Sudeep Agrawal <skagrawal@kimcorealty.com>
2017-11-12 18:06:52 -0500

    Completed code function to read Excelfile from folder location.

---
+++ ReadExcel/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ReadExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxSheet = new System.Windows.Forms.ComboBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.LabelSheet = new System.Windows.Forms.Label();
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonDownloadFolder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxSheet
+            // 
+            this.comboBoxSheet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSheet.FormattingEnabled = true;
+            this.comboBoxSheet.Location = new System.Drawing.Point(118, 22);
+            this.comboBoxSheet.Name = "comboBoxSheet";
+            this.comboBoxSheet.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxSheet.TabIndex = 0;
+            this.comboBoxSheet.SelectedIndexChanged += new System.EventHandler(this.combobox_Sheet_selectedItem);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 59);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(958, 480);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(12, 545);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(83, 31);
+            this.buttonOpen.TabIndex = 3;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LabelSheet
+            // 
+            this.LabelSheet.AutoSize = true;
+            this.LabelSheet.Location = new System.Drawing.Point(17, 25);
+            this.LabelSheet.Name = "LabelSheet";
+            this.LabelSheet.Size = new System.Drawing.Size(49, 17);
+            this.LabelSheet.TabIndex = 5;
+            this.LabelSheet.Text = "Sheet:";
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Location = new System.Drawing.Point(773, 545);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(197, 31);
+            this.buttonDownload.TabIndex = 6;
+            this.buttonDownload.Text = "Start Downloading Images";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(262, 545);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(478, 30);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(160, 550);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Progress:";
+    
+            // 
+            // buttonDownloadFolder
+            // 
+            this.buttonDownloadFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDownloadFolder.Location = new System.Drawing.Point(737, 12);
+            this.buttonDownloadFolder.Name = "buttonDownloadFolder";
+            this.buttonDownloadFolder.Size = new System.Drawing.Size(233, 37);
+            this.buttonDownloadFolder.TabIndex = 9;
+            this.buttonDownloadFolder.Text = "Select Download Folder";
+            this.buttonDownloadFolder.UseVisualStyleBackColor = true;
+            this.buttonDownloadFolder.Click += new System.EventHandler(this.buttonDownloadFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 586);
+            this.Controls.Add(this.buttonDownloadFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonDownload);
+            this.Controls.Add(this.LabelSheet);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.comboBoxSheet);
+            this.Name = "Form1";
+            this.Text = "Image Downloader";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxSheet;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Label LabelSheet;
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonDownloadFolder;
+    }
+}
+
---
+++ ReadExcel/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Windows.Forms;
+using Excel;
+
+namespace ReadExcel
+{
+    public partial class Form1 : Form
+    {
+        private String destinationFolder = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataSet result;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Workbook | *.xls", ValidateNames = true })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileStream fs = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read,FileShare.ReadWrite);
+                    IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(fs);
+                    excelReader.IsFirstRowAsColumnNames = true;
+                    result = excelReader.AsDataSet();
+                    comboBoxSheet.Items.Clear();
+                    foreach (DataTable dt in result.Tables)
+                    {
+                        comboBoxSheet.Items.Add(dt.TableName);
+                    }//for
+                    excelReader.Close();
+                }//if
+            }
+        }
+
+        private void combobox_Sheet_selectedItem(object sender, EventArgs e)
+        {
+            dataGridView.DataSource = result.Tables[comboBoxSheet.SelectedIndex];
+        }//combobox_Shet_selectedItem
+
+
+        private void buttonDownloadFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd.ShowNewFolderButton = true;
+            fbd.Description = "Choose folder to download all images.";
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                destinationFolder = fbd.SelectedPath+"\\";
+            }//if
+        }//buttonDownloadFolder_Click
+    }
+}
